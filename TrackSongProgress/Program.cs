40909d67c6ef5a75891d121879a46deb977b1ca5
cade-perfili <58614773+cade-perfili@users.noreply.github.com>
2022-03-05 08:35:25 -0500

    Trial and error, master loop

--- TrackSongProgress/Program.cs
+++ TrackSongProgress/Program.cs
@@ -1,33 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Song Tracker!");
-Console.WriteLine("What would you like to do today?");
-Console.WriteLine("Enter 1. To add a new song");
-Console.WriteLine("Enter 2. To Review Current Song");
-Console.WriteLine("enter 3. To Exit the Program and get some practice!");
 
-string menu = Console.ReadLine();
 
-bool go = true;
-string NewSong;
+    Console.WriteLine("Welcome to Song Tracker!");
+    Console.WriteLine("What would you like to do today?");
+    Console.WriteLine("Enter 1. To add a new song");
+    Console.WriteLine("Enter 2. To Review Current Song");
+    Console.WriteLine("enter 3. To Exit the Program and get some practice!");
 
-while (go)
+    string menu = Console.ReadLine();
+
+    bool go = true;
+    string NewSong;
+//Master Loop Menu
+    while (go)
     {
- switch(menu)
+        switch (menu)
         {
-        case "1":
+            case "1":
+
+                Console.WriteLine("Please enter the song name, press 1 to return to main menu");
+                NewSong = Console.ReadLine();
+            Console.WriteLine($"{NewSong}, Has been entered, thank you!");
+            if (NewSong == "1"){
+                return;
+            }
+            goto case "3";
+
+            break;
+                
+
+            case "3":
             
-            Console.WriteLine("Please enter the song name, press 1 to return to main menu");
-            NewSong = Console.ReadLine();
-            Console.WriteLine(NewSong);
-            break;
+            default:
+                go = false;
+                break;
+        };
 
-        case "3":
 
-        default:
-            go = false;
-            break;
+
     };
-
-    
-
-};
